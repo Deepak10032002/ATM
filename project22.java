5fce1d67253fcb557fe6dc3c75ed616480506334
Deepak10032002 <121559205+Deepak10032002@users.noreply.github.com>
2022-12-28 19:53:04 +0530

    first commit

---
+++ project22.java
@@ -0,0 +1,11 @@
+package ATM;
+import java.io.IOException;
+
+class project22 extends OptionMenu {
+    public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+
+}
